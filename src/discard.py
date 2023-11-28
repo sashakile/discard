110ecccf7e4baba291c30169e670fc3cf1540ae5
sashakile <saxakile@gmail.com>
2023-11-27 22:55:46 -0300

    add iteration through all files to get annotations

--- src/discard.py
+++ src/discard.py
@@ -1,14 +1,21 @@
 from pathlib import Path
 from itertools import islice
 from json import load
+from pprint import pprint
 
 HOME_PATH = Path("/mnt/c/Users/augus/Downloads/takeout-20230823T012425Z-001/Takeout/Keep/")
 
-for path in islice(HOME_PATH.glob("*.json"), 1):
-    with open(path) as f:
+def get_annotations(path):
+    with open(path) as f:                                
         content = load(f)
+        annotations = content.get("annotations",[])
+        yield from annotations            
 
-content
+
+for path in islice(HOME_PATH.glob("*.json"), 100):
+    for ann in get_annotations(path):
+        pprint(ann)
+
 
 # discard add url
 # discard import file.json
