b87780621c079838684aa8358babd2a0f96e7cd9
sashakile <saxakile@gmail.com>
2023-11-27 23:24:09 -0300

    refactor extraction of content from files

--- src/discard.py
+++ src/discard.py
@@ -3,17 +3,22 @@ from itertools import islice
 from json import load
 from pprint import pprint
 
-HOME_PATH = Path("/mnt/c/Users/augus/Downloads/takeout-20230823T012425Z-001/Takeout/Keep/")
+HOME_PATH = Path(
+    "/mnt/c/Users/augus/Downloads/takeout-20230823T012425Z-001/Takeout/Keep/")
 
-def get_annotations(path):
-    with open(path) as f:                                
+
+def get_content(path):
+    with open(path) as f:
         content = load(f)
-        annotations = content.get("annotations",[])
-        yield from annotations            
+        return content
+
+def get_annotations(content):
+    annotations = content.get("annotations", [])
+    yield from annotations
 
 
 for path in islice(HOME_PATH.glob("*.json"), 100):
-    for ann in get_annotations(path):
+    for ann in get_annotations(get_content(path)):
         pprint(ann)
 
 
